08ece71185c704eb7ab1262622add64c3b9c2259
Stefan Penk <stefan.penk@gmail.com>
2022-10-19 07:48:43 +0200

    video poster added, article responsive added

--- src/components/_Home/HeroSection/hero-section.component.tsx
+++ src/components/_Home/HeroSection/hero-section.component.tsx
@@ -10,6 +10,7 @@ const HeroSection = () => {
         autoPlay
         loop
         muted
+        poster="https://firebasestorage.googleapis.com/v0/b/foocoding-react-project.appspot.com/o/WebsiteImgs%2Fhero-img.webp?alt=media&token=29c495d6-d2af-4f52-bbe0-53c7a46e11b0"
       />
       <div className="hero_text">
         <h1>DON'T THROW AWAY FOOD</h1>
